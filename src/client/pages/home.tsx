3567103f720e65aa670c0e63f2907110d81449ac
Csaba Apagyi <csaba.apagyi@gmail.com>
2021-04-11 23:15:04 +0200

    Fix jsx lint

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { NextPage } from 'next'
+import React from 'react';
+import { NextPage } from 'next';
 
 const Home: NextPage = () => {
- return <h1>Hello from NextJS! - Home</h1>
-}
- 
-export default Home
+  return <h1>Hello from NextJS! - Home</h1>;
+};
+
+export default Home;
