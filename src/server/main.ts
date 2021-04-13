70c6baf244d7eaaeef8619be236031b7fe68b6a7
Csaba Apagyi <csaba.apagyi@gmail.com>
2021-04-13 23:42:37 +0200

    Listen on PORT env

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,6 @@ import { ServerModule } from 'src/server/server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
