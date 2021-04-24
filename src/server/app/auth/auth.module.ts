1c13464cd22a97552e1786c12468efbe57163d3f
Csaba Apagyi <csaba.apagyi@gmail.com>
2021-04-24 18:13:43 +0200

    Add /auth endpoint

--- src/server/app/auth/auth.module.ts
+++ src/server/app/auth/auth.module.ts
@@ -3,8 +3,10 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { GoogleOauthModule } from './google/google-oauth.module';
 import { JwtAuthModule } from './jwt/jwt-auth.module';
+import { AuthController } from './auth.controller';
 
 @Module({
+  controllers: [AuthController],
   imports: [UsersModule, PassportModule, GoogleOauthModule, JwtAuthModule],
 })
 export class AuthModule {}
