9df18137ea88c2835ed2c9432ef5706b2d6601ba
Csaba Apagyi <csaba.apagyi@gmail.com>
2021-04-24 19:52:05 +0200

    Showcase initial server side props for NextJS

--- src/client/pages/home.tsx
+++ src/client/pages/home.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <h1>Hello from NextJS! - Home</h1>;
+const Home: NextPage<{ data: string }> = (props) => {
+  const { data } = props;
+
+  return (
+    <div>
+      <h1>Hello from NextJS! - Home</h1>
+      {data}
+    </div>
+  );
+};
+
+Home.getInitialProps = ({ query }) => {
+  return {
+    data: `some initial props including query params and controller data: ${JSON.stringify(
+      query,
+    )}`,
+  };
 };
 
 export default Home;
