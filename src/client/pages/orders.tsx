b16c836903f6697760ec69877c07264b59ab081a
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-03-27 21:48:11 +0200

    Simplify typedQuery to return client.query

--- src/client/pages/orders.tsx
+++ src/client/pages/orders.tsx
@@ -5,7 +5,7 @@ import { Request } from 'express';
 import { typedQuery } from '../app/apollo-client';
 
 export async function getServerSideProps({ req }) {
-  const data = await typedQuery(
+  const { data } = await typedQuery(
     { orders: { alias: true, thing: { name: true } } },
     req,
   );
