8b9061cedfcf961b3d966f0605b60be2f9173a73
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-07-24 14:42:10 +0200

    Fix migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,19 +1,19 @@
+import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export = {
+const source = new DataSource({
   type: 'postgres' as const,
   url: process.env.DATABASE_URL,
   entities: ['src/server/app/**/*.entity.ts'],
   migrations: ['src/server/migration/*.{ts,js}'],
-  cli: {
-    migrationsDir: 'src/server/migration',
-  },
   extra: {
     ssl:
       process.env.NODE_ENV === 'production'
         ? { rejectUnauthorized: false }
         : false,
   },
-};
+});
+
+export default source;
