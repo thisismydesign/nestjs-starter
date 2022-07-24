c0017b5c06934866ce3fe64bcf8bc9704acde53c
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-07-24 23:38:51 +0200

    Update auto-generated gql schema

--- src/client/app/types/zeus/apollo.ts
+++ src/client/app/types/zeus/apollo.ts
@@ -1,28 +1,46 @@
 /* eslint-disable */
 
-import { Zeus, GraphQLTypes, InputType, ValueTypes } from './index';
+import { Zeus, GraphQLTypes, InputType, ValueTypes, OperationOptions, ScalarDefinition } from './index';
 import { gql, useQuery, useLazyQuery, useMutation } from '@apollo/client';
 import type { QueryHookOptions, LazyQueryHookOptions, MutationHookOptions } from '@apollo/client';
 
 
-export function useTypedQuery<Z extends ValueTypes[O], O extends "Query">(
+export function useTypedQuery<Z extends ValueTypes[O], O extends "Query", SCLR extends ScalarDefinition>(
   query: Z | ValueTypes[O],
-  options?: QueryHookOptions<InputType<GraphQLTypes[O], Z>>,
-  operationName?: string,
+  options?:{
+    apolloOptions?: QueryHookOptions<InputType<GraphQLTypes[O], Z, SCLR>>,
+    operationOptions?: OperationOptions,
+    scalars?: SCLR
+  }
 ) {
-  return useQuery<InputType<GraphQLTypes[O], Z>>(gql(Zeus("query",query, operationName)), options);
+  return useQuery<InputType<GraphQLTypes[O], Z, SCLR>>(gql(Zeus("query",query, {
+    operationOptions: options?.operationOptions,
+    scalars: options?.scalars
+  })), options?.apolloOptions);
 }
-export function useTypedLazyQuery<Z extends ValueTypes[O], O extends "Query">(
+export function useTypedLazyQuery<Z extends ValueTypes[O], O extends "Query", SCLR extends ScalarDefinition>(
   LazyQuery: Z | ValueTypes[O],
-  options?: LazyQueryHookOptions<InputType<GraphQLTypes[O], Z>>,
-  operationName?: string,
+  options?:{
+    apolloOptions?: LazyQueryHookOptions<InputType<GraphQLTypes[O], Z, SCLR>>,
+    operationOptions?: OperationOptions,
+    scalars?: SCLR
+  }
 ) {
-  return useLazyQuery<InputType<GraphQLTypes[O], Z>>(gql(Zeus("query",LazyQuery, operationName)), options);
+  return useLazyQuery<InputType<GraphQLTypes[O], Z, SCLR>>(gql(Zeus("query",LazyQuery, {
+    operationOptions: options?.operationOptions,
+    scalars: options?.scalars
+  })), options?.apolloOptions);
 }
-export function useTypedMutation<Z extends ValueTypes[O], O extends "Mutation">(
+export function useTypedMutation<Z extends ValueTypes[O], O extends "Mutation", SCLR extends ScalarDefinition>(
   mutation: Z | ValueTypes[O],
-  options?: MutationHookOptions<InputType<GraphQLTypes[O], Z>>,
-  operationName?: string,
+  options?:{
+    apolloOptions?: MutationHookOptions<InputType<GraphQLTypes[O], Z, SCLR>>,
+    operationOptions?: OperationOptions,
+    scalars?: SCLR
+  }
 ) {
-  return useMutation<InputType<GraphQLTypes[O], Z>>(gql(Zeus("mutation",mutation, operationName)), options);
+  return useMutation<InputType<GraphQLTypes[O], Z, SCLR>>(gql(Zeus("mutation",mutation, {
+    operationOptions: options?.operationOptions,
+    scalars: options?.scalars
+  })), options?.apolloOptions);
 }
