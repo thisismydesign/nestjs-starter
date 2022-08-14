c0d3039f12460bb5e5d76a5dfb638e2ccade4c29
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-08-14 23:28:54 +0200

    Make GA script loading optional

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -5,21 +5,24 @@ import Script from 'next/script';
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <div className="container">
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-        </Script>
-      </div>
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+  `}
+          </Script>
+        </>
+      )}
+
       <Component {...pageProps} />
     </>
   );
